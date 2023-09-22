c3b5f6df3fc58ee48e80162b065bb5a09f4773aa
Philipp Mildenberger <philipp@mildenberger.me>
2023-09-23 00:56:13 +0200

    Added a convenient script with X11 support via docker and nvidia for viewing models

---
+++ view.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+docker build -t gaussian-splatting-3d .
+xhost local:root
+docker run --gpus 1 --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/models:/workspace/gaussian-splatting/models \
+                      -e "DISPLAY=:0" -e "NVIDIA_DRIVER_CAPABILITIES=compute,utility,graphics" gaussian-splatting-3d:latest ./SIBR_viewers/install/bin/SIBR_gaussianViewer_app $@
